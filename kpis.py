983e3b3e1c0d6b56e94469b628b8411c3a345e3c
Timo Kubitza <timo.kubitza@lana-labs.com>
2021-09-21 08:01:46 +0200

    add new function

--- kpis.py
+++ kpis.py
@@ -1,5 +1,21 @@
+from datetime import time
+from pandas.core.frame import DataFrame
 from dashboard_components.api_abstraction import aggregate
 import pandas as pd
+from functools import lru_cache
+
+
+def fix_dates(df):
+    if "byQuarter" in df.columns:
+        df['x_axes'] = pd.to_datetime(
+            df['byQuarter'].str.replace(r'(Q\d) (\d+)', r'\2-\1'), errors='coerce')
+    elif "byYear" in df.columns:
+        df["x_axes"] = pd.to_datetime(df.byYear)
+    elif "byMonth" in df.columns:
+        df["x_axes"] = pd.to_datetime(df.byMonth)
+
+    df = df.sort_values("x_axes", ascending=True)
+    return df
 
 
 def number_of_cases(log_id, api_key, tfs=[]) -> int:
@@ -87,8 +103,8 @@ def cases_per_weekday(log_id, api_key, tfs=[]) -> pd.DataFrame:
 
     return df_comb
 
-def activity_per_timeUnit(api_key, log_id, tfs, activities: list, frequency) -> pd.DataFrame:
-    if frequency not in ["byYear", "byMonth", "byQuarter", "byDayOfWeek", "byDayOfYear", "byHourOfDay"]:
+def activity_per_time_unit(api_key, log_id, tfs, activities: list, time_unit: str) -> pd.DataFrame:
+    if time_unit not in ["byYear", "byMonth", "byQuarter", "byDayOfWeek", "byDayOfYear", "byHourOfDay"]:
         raise Exception("Frequency Parameter not valid. Needs to be in: \
                     [ byYear, byMonth, byQuarter, byDayOfWeek, byDayOfYear, byHourOfDay ]")
 
@@ -96,10 +112,47 @@ def activity_per_timeUnit(api_key, log_id, tfs, activities: list, frequency) -> 
                    log_id=log_id,
                    trace_filter_sequence=tfs,
                    metric="frequency",
-                   grouping="byMonth",
+                   grouping=time_unit,
                    date_type="startDate",
                    secondary_grouping="byActivity",
                    secondary_activities=activities,
                    values_from="allEvents"
                    )
     return df
+
+
+@lru_cache(maxsize=5)
+def cases_per_time_unit(api_key, log_id, tfs, time_unit: str) -> pd.DataFrame:
+    if time_unit not in ["byYear", "byMonth", "byQuarter", "byDayOfWeek", "byDayOfYear", "byHourOfDay"]:
+        raise Exception("Frequency Parameter not valid. Needs to be in: \
+                    [ byYear, byMonth, byQuarter, byDayOfWeek, byDayOfYear, byHourOfDay ]")
+
+    df = aggregate(api_key=api_key,
+                   log_id=log_id,
+                   trace_filter_sequence=tfs,
+                   metric="frequency", 
+                   grouping=time_unit,
+                   date_type="startDate", 
+                   max_amount_attributes=10000,
+                   values_from="allCases")
+    df = fix_dates(df)
+    return df
+
+
+@lru_cache(maxsize=5)
+def avg_growth_rate(api_key, log_id, tfs, time_unit: str, time_length: int = 12) -> float:
+    df = aggregate(api_key=api_key,
+                   log_id=log_id,
+                   trace_filter_sequence=tfs,
+                   metric="frequency", 
+                   grouping=time_unit,
+                   date_type="startDate", 
+                   max_amount_attributes=10000,
+                   values_from="allCases")
+    df = fix_dates(df)
+    df = df.sort_values("x_axes", ascending=True)
+
+    df["C"] = df.frequency.pct_change().mul(100).round(2)
+    length = - time_length
+    avg_gr = df["C"][length:].mean()
+    return round(avg_gr, 2)
